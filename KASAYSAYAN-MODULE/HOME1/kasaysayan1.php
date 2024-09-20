bcb216952dbeb925f863e6893112b0856189d6f1
eysicoco <allencandelaria23@gmail.com>
2024-09-20 22:02:01 +0800

    Add files via upload

---
+++ KASAYSAYAN-MODULE/HOME1/kasaysayan1.php
@@ -0,0 +1,309 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../HOME1/welcome1.css">
+    <title>Philippine History Module</title>
+</head>
+
+<body>
+
+<!-- Top Header -->
+			<header id="header">
+			<div class="container">
+			<button id="sidebarToggle"><img src="../PICS/sidenav1.png" alt="Menu"></button>
+			
+            <div class="logo">
+            <img src="../PICS/logo1.png" alt="Logo">
+            </div>
+			
+            <h1>DISCOVERING PHILIPPINE HISTORY</h1>
+			
+<!-- Search -->
+            <div class="search-bar">
+			
+			<button class="search-img"><img src="../PICS/search.png" alt="Search"></button>
+			<input type="text" placeholder="Maghanap...">
+			<button class="info-btn"><img src="../PICS/info.png" alt="Info"></button>
+			
+			</div>
+            </div>
+            </header>
+
+<!-- Table of Contents Sidebar -->
+			<nav id="navSidebar">
+			
+			<ul>
+			<li><label class="label1"> TALAAN NG NILALAMAN</label></li><hr>
+			
+            <li>
+    <a href="#mainContent">YUNIT 1</a>
+    <!-- Toggle dropdown buttons -->
+    <div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+</li>
+			
+			<li>
+			<a href="#TOPIC 1">YUNIT 2</a>
+			 <div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+	
+			<li>
+			<a href="#TOPIC 2">YUNIT 3</a>
+			<div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+	
+			<li>
+			<a href="#TOPIC 3">YUNIT 4</a>
+			<div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+			
+			<li>
+			<a href="#TOPIC 4">YUNIT 5</a>
+			<div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+			
+			<li>
+			<a href="#TOPIC 5">YUNIT 6</a>
+			<div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+			
+			<li>
+			<a href="#TOPIC 6">YUNIT 7</a>
+			<div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+			
+			<li>
+			<a href="#TOPIC 7">YUNIT 8</a>
+			<div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+			
+			<li>
+			<a href="#TOPIC 8">YUNIT 9</a>
+			<div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+			
+			<li>
+			<a href="#TOPIC 9">YUNIT 10</a>
+			<div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+			
+			<li>
+			<a href="#TOPIC 10">YUNIT 11</a>
+			<div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+			
+			<li>
+			<a href="#TOPIC 11">YUNIT 12</a>
+			<div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+			
+			<li>
+			<a href="#TOPIC 12">YUNIT 13</a>
+			<div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+			
+			<li>
+			<a href="#TOPIC 13">YUNIT 14</a>
+			<div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+			
+			<li>
+			<a href="#TOPIC 14">YUNIT 15</a>
+			<div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+			  
+			</ul>
+			
+			</nav>
+	
+    
+<!-- User Navigation Sidebar -->
+			<aside id="sidebar">
+			<nav>
+			
+			<div class="user-icon">
+			<img src="../PICS/haerin.jpg" alt="User Icon" id="userIcon">>
+			</div>
+	
+			<div class="username">
+			<p>username</p>
+			</div>
+    
+			 <div class="user-actions">
+			<label for="editUserIconInput" id="editUserIconLabel">Edit Icon</label>
+            <input type="file" id="editUserIconInput" accept="image/*"></input>
+			</div>
+			
+			<ul>
+			<li><a href="../HOME1/kasaysayan1.php">HOME</a></li>
+			<li><a href="../GALLERY/gallery.php">GALLERY</a></li>
+            <li><a href="../QUIZ1/quiz1.php">TAKE A QUIZ</a></li>
+            <li><a href="../LOGIN1/Login1.php">LOGOUT</a></li>
+			</ul>
+			
+			<button id="closeSidebar">Close</button>
+			</nav>
+			</aside>
+
+<!-- CONTENTS -->
+			<main id="mainContent">
+			<section class="content-section">
+			
+			<div class="content-item">
+			<img src="../PICS/unit 1 pics/01-rizal.jpg" alt="Image 1">
+			<p id="TOPIC 1"><b>YUNIT 1:</b><br>
+			Mga Salik na Nag-ambag sa Pagiging Dakilang Tao at Bayani ni Rizal </p>
+			</div>
+			
+			<div class="content-item">
+			<img src="../PICS/unit 2 pics/1.jfif" alt="Image 2">
+			<p id="TOPIC 2"><b>YUNIT 2:</b><br>
+			Ang kalagayan ng Pilipinas noong ika-19 na siglo: Isang Panoramic Survey</p>
+			</div>
+			
+			<div class="content-item">
+			<img src="../PICS/WT.jpg" alt="Image 3">
+			<p id="TOPIC 3"><b>YUNIT 3:</b><br>
+		    Buhay ni Rizal: Pamilya, Pagkabata at Edukasyon</p>
+			</div>
+			
+			<div class="content-item">
+			<img src="../PICS/WT.jpg" alt="Image 1">
+			<p id="TOPIC 4"><b>YUNIT 4:</b><br>
+			Buhay ni Rizal: Mataas na Edukasyon at Buhay sa Ibang Bansa</p>
+			</div>
+			
+			<div class="content-item">
+			<img src="../PICS/WT.jpg" alt="Image 2">
+			<p id="TOPIC 5"><b>LESSON 5: BATTLE OF YULTONG</b><br>
+			Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description </p>
+			</div>
+			
+			<div class="content-item">
+			<img src="../PICS/ktz.jpg" alt="Image 3">
+			<p id="TOPIC 6"><b>LESSON 6: CULLING GAMES</b><br>
+			Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description </p>
+			</div>
+			
+			<div class="content-item">
+			<img src="../PICS/kyd-tnz.jpg" alt="Image 1">
+			<p id="TOPIC 7"><b>LESSON 7: CULLING GAMES</b><br>
+			Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description </p>
+			</div>
+			
+			<div class="content-item">
+			<img src="../PICS/kyd-tnz.jpg" alt="Image 2">
+			<p id="TOPIC 8"><b>LESSON 8: CULLING GAMES</b><br>
+			Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description </p>
+			</div>
+			
+			<div class="content-item">
+			<img src="../PICS/ktz.jpg" alt="Image 3">
+			<p id="TOPIC 9"><b>LESSON 9: CULLING GAMES</b><br>
+			Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description </p>
+			</div>
+			
+			<div class="content-item">
+			<img src="../PICS/kyd-tnz.jpg" alt="Image 1">
+			<p id="TOPIC 10"><b>LESSON 10: CULLING GAMES</b><br>
+			Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description </p>
+			</div>
+			
+			<div class="content-item">
+			<img src="../PICS/ktz.jpg" alt="Image 2">
+			<p id="TOPIC 11"><b>LESSON 11: SHIBUYA INCIDENT</b><br>
+			Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description </p>
+			</div>
+			
+			<div class="content-item">
+			<img src="../PICS/ktz.jpg" alt="Image 3">
+			<p id="TOPIC 12"><b>LESSON 12: SHIBUYA INCIDENT</b><br>
+			Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description </p>
+			</div>
+			
+			<div class="content-item">
+			<img src="../PICS/kyd-tnz.jpg" alt="Image 1">
+			<p id="TOPIC 13"><b>LESSON 13: SHIBUYA INCIDENT</b><br>
+			Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description </p>
+			</div>
+			
+			<div class="content-item">
+			<img src="../PICS/ktz.jpg" alt="Image 2">
+			<p id="TOPIC 14"><b>LESSON 14: SHIBUYA INCIDENT</b><br>
+			Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description </p>
+			</div>
+			
+			<div class="content-item">
+			<img src="../PICS/ktz.jpg" alt="Image 3">
+			<p id="TOPIC 15"><b>LESSON 15: SHIBUYA INCIDENT</b><br>
+			Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description </p>
+			</div>
+
+			</section>
+			</main>
+
+
+    <script src="homejs.js"></script>
+</body>
+</html>
---
+++ KASAYSAYAN-MODULE/HOME1/unit1.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../HOME1/unit1-css.css">
+    <title>UNIT 1</title>
+</head>
+
+<body>
+
+<!-- Top Header -->
+			<header id="header">
+			<div class="container">
+			<button id="sidebarToggle"><img src="../PICS/sidenav1.png" alt="Menu"></button>
+			
+			
+            <div class="logo">
+            <img src="../PICS/logo1.png" alt="Logo">
+            </div>
+			
+            <h1>DISCOVERING PHILIPPINE HISTORY</h1>
+			<div class="notebtn">
+			<button id="noteOptionBtn">
+			<img src="../PICS/note.png" alt="Note Icon">
+			</button></div>
+<!-- Search -->
+            <div class="search-bar">
+			
+			<button class="search-img"><img src="../PICS/search.png" alt="Search"></button>
+			<input type="text" placeholder="Maghanap...">
+			<button class="info-btn"><img src="../PICS/info.png" alt="Info"></button>
+			
+			</div>
+            </div>
+            </header>
+			
+<!-- Table of Contents Sidebar -->
+			<nav id="navSidebar">
+			
+			<ul>
+			<li><label class="label1"> TALAAN NG NILALAMAN</label></li><hr>
+			
+            <li class="unit1h">
+    <a class="unit1hh" href="#mainContent">YUNIT 1</a>
+    <div class="topic-buttons1" id="tpc-btn1">
+        <a href="#mainContent"><button class="dropdown-btn">REPUBLIC ACT NO. 1425</button></a>
+        <a href="#sbt2"><button class="dropdown-btn">RIZAL: INSPIRASYON NG BAGONG HENERASYON</button></a>
+        <a href="#sbt3"><button class="dropdown-btn">BATAS RIZAL: KAPARIS NG RH BILL</button></a>
+		<a href="#sbt4"><button class="dropdown-btn">BATAS RIZAL: SA NAKARAANG 50 TAON</button></a>
+    </div>
+</li>
+			
+			<li>
+			<a href="#TOPIC 1">YUNIT 2</a>
+			 <div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+	
+			<li>
+			<a href="#TOPIC 2">YUNIT 3</a>
+			<div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+	
+	 <li>
+    <a href="#mainContent">YUNIT 4</a>
+    <div class="topic-buttons">
+        <a href="#mainContent"><button class="dropdown-btn">Subtopic 1</button></a>
+        <a href="#sbt1"><button class="dropdown-btn">Subtopic 2</button></a>
+        <a href="#sbt3"><button class="dropdown-btn">Subtopic 3</button></a>
+		<a href="#sbt4"><button class="dropdown-btn">Subtopic 4</button></a>
+    </div>
+</li>
+			
+			<li>
+			<a href="#TOPIC 1">YUNIT 5</a>
+			 <div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+	
+			<li>
+			<a href="#TOPIC 2">YUNIT 6</a>
+			<div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+	
+	 <li>
+    <a href="#mainContent">YUNIT 7</a>
+    <div class="topic-buttons">
+        <a href="#mainContent"><button class="dropdown-btn">Subtopic 1</button></a>
+        <a href="#sbt1"><button class="dropdown-btn">Subtopic 2</button></a>
+        <a href="#sbt3"><button class="dropdown-btn">Subtopic 3</button></a>
+		<a href="#sbt4"><button class="dropdown-btn">Subtopic 4</button></a>
+    </div>
+</li>
+			
+			<li>
+			<a href="#TOPIC 1">YUNIT 8</a>
+			 <div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+	
+			<li>
+			<a href="#TOPIC 2">YUNIT 9</a>
+			<div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+	
+	 <li>
+    <a href="#mainContent">YUNIT 10</a>
+    <div class="topic-buttons">
+        <a href="#mainContent"><button class="dropdown-btn">Subtopic 1</button></a>
+        <a href="#sbt1"><button class="dropdown-btn">Subtopic 2</button></a>
+        <a href="#sbt3"><button class="dropdown-btn">Subtopic 3</button></a>
+		<a href="#sbt4"><button class="dropdown-btn">Subtopic 4</button></a>
+    </div>
+</li>
+			
+			<li>
+			<a href="#TOPIC 1">YUNIT 11</a>
+			 <div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+	
+			<li>
+			<a href="#TOPIC 2">UNIT 12</a>
+			<div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+	
+	 <li>
+    <a href="#mainContent">UNIT 13</a>
+    <div class="topic-buttons">
+        <a href="#mainContent"><button class="dropdown-btn">Subtopic 1</button></a>
+        <a href="#sbt1"><button class="dropdown-btn">Subtopic 2</button></a>
+        <a href="#sbt3"><button class="dropdown-btn">Subtopic 3</button></a>
+		<a href="#sbt4"><button class="dropdown-btn">Subtopic 4</button></a>
+    </div>
+</li>
+			
+			<li>
+			<a href="#TOPIC 1">UNIT 14</a>
+			 <div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+	
+			<li>
+			<a href="#TOPIC 2">UNIT 15</a>
+			<div class="topic-buttons">
+        <button class="dropdown-btn">Subtopic 1</button>
+        <button class="dropdown-btn">Subtopic 2</button>
+        <button class="dropdown-btn">Subtopic 3</button>
+    </div>
+	</li>
+	
+			  
+			</ul>
+			
+			</nav>
+	
+    
+<!-- User Navigation Sidebar -->
+			<aside id="sidebar">
+			<nav>
+			
+			<div class="user-icon">
+			<img src="../PICS/haerin.jpg" alt="User Icon" id="userIcon">>
+			</div>
+	
+			<div class="username">
+			<p>username</p>
+			</div>
+    
+			 <div class="user-actions">
+			<label for="editUserIconInput" id="editUserIconLabel">Edit Icon</label>
+            <input type="file" id="editUserIconInput" accept="image/*"></input>
+			</div>
+			
+			<ul>
+			<li><a href="../HOME1/kasaysayan1.php">HOME</a></li>
+			<li><a href="../GALLERY/gallery.php">GALLERY</a></li>
+            <li><a href="../QUIZ1/quiz1.php">TAKE A QUIZ</a></li>
+            <li><a href="../LOGIN1/Login1.php">LOGOUT</a></li>
+			</ul>
+			
+			<button id="closeSidebar">Close</button>
+			</nav>
+			</aside>
+	
+<!-- CONTENTS -->	
+			<main id="mainContent">
+    <section class="content-section">
+	 
+		 <h1 id="sbt1"><span class="bookmark-icon"></span>REPUBLIC ACT NO. 1425</h1>
+		 <img src="../PICS/unit 1 pics/RA1.png" alt="Image Description">
+		 <p>Ang Batas Republika 1425 na mas kilala sa tawag na Batas Rizal ay pinangunahan ng dating pinuno ng Pambansang Kapulungan ng Edukasyon na si Sen. Jose P. Laurel. Bago ito mapagtibay noong Hunyo 12, 1956, dumaan ang batas na ito sa mga umaatikabong debate sa loob ng Senado at Kongreso.<br><br>
+		 <p>Tinawag itong House Bill 5561 sa kongreso na pinangunahan ni Cong. Jacobo Gonzales at tinawag naman itong Senate Bill 438 sa Senado na pinangunahan naman ni Sen. Claro M. Recto. Hindi makakapagtaka na sila ang mga pinunong nagtaguyod sa batas na ito, dahil kung babalikan ang kasaysayan, malinaw na may marubdob na pagmamahal sa bayan ang dalawang ito. Si Gonzales ay nakipaglaban upang mapalaya ang kanyang mga kababayang sakdalista at si Recto naman ay malinaw na ipinaglaban ang soberanya ng Pilipinas labas sa Estados Unidos.</p>
+
+</p><br><br><h1 id="sbt2"><span class="bookmark-icon"></span>RIZAL: INSPIRASYON NG BAGONG HENERASYON</h1><p>
+Ang pangunahing layunin ng mga mga nagtaguyod sa batas na ito ay muling pag-alabin ang diwa ng nasyonalismo sa mga susunod na henerasyon ng mga Pilipino sa pamamagitan ng pagsama sa kurikulum ng lahat ng paaralan, pampubliko man o pribado ang kurso sa pag-aaral ng buhay, mga ginawa at isinulat ni Jose Rizal, partikular na ang kanyang dalawang nobela na Noli Me Tangere at El Filibusterismo. Naniniwala sila na si Rizal ay maaaring magsilbing inspirasyon sa atin, lalo na sa mga kabataan. Bukod dito, layunin din ng batas na ito na parangalan si Rizal at ang iba pa nating mga bayani sa lahat ng kanilang mga ginawa para sa bayan.
+
+</p><br><br><h1 id="sbt3"><span class="bookmark-icon"></span>BATAS RIZAL: KAPARIS NG RH BILL</h1><p>
+Katulad ng RH Bill, hindi naging madali ang pagpasa ng batas na ito dahil sa malakas na pagtutol ng Simbahang Katoliko. Naniniwala ang simbahan at ang mga mambabatas na pumanig sa kanila na ang dalawang nobela ni Rizal ay naglalaman ng mga pahayag na laban sa simbahan. Ayon sa kanila, ang sinumang makabasa ng Noli Me Tangere at El Filibusterismo ay maaaring mawalan ng pananampalataya o sumalungat sa mga itinuturo ng simbahan.Iba’t ibang taktika ang ginamit nila upang takutin ang mga nagtataguyod sa batas na ito. Nariyang magsasara daw ang lahat ng paaaralang pag-aari ng simbahan sa oras na maipatupad ito at hindi daw makakaasa ng suporta mula sa mga katoliko ang mga nagtaguyod nito sa mga susunod na halalan. Ngunit kaparis ng RH Bill, naipatupad ang Batas Rizal sa kabila ng malakas na pagtutol ng simbahan
+
+</p><br><br><h1 id="sbt4"><span class="bookmark-icon"></span>BATAS RIZAL: SA NAKARAANG 50 TAON</h1>
+<p>Limang dekada na ang nakalipas mula ng maipatupad ang Batas Rizal. Ngunit nakatulong nga ba ang pag-aaral ng buhay, mga ginawa at isinulat ni Jose Rizal upang makamit ang mga adhikain ng mga nagtaguyod sa batas na ito? Sa mababaw na pagtingin, masasabi nating naging matagumpay ito, dahil sa halos lahat ng plaza sa ating bansa ay may bantayog si Rizal. Kilala siya ng lahat ng mga Pilipino bilang ating pambansang bayani. Madalas din nating gamitin ang mga salitang iniwan niya, katulad ng walang-kamatayang “ang kabataan ang pag-asa ng bayan.”
+</p>
+
+
+		 
+		 
+	 </section>
+</main>
+
+<!-- Note Container -->
+
+<!-- Add this div at the bottom of the body, above the script tags -->
+<div id="noteContainer" style="display: none;">
+    <textarea id="noteTextarea" placeholder="Isulat ang iyong tala dito..."></textarea>
+    <button id="saveNoteBtn">I-save</button>
+    <button id="cancelNoteBtn">Kanselahin</button>
+</div>
+
+
+
+    <script src="../HOME1/homejs.js"></script>
+</body>
+</html>
